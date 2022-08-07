1d2e4b161ab02875d55b9d126ab8cf457e64cd63
ViNeeTh <37704366+vineethbhandary@users.noreply.github.com>
2022-08-07 12:43:22 +0530

    TESTUNG NEW COMMIT
    HELLO JAVA

---
+++ MyJava.java
@@ -0,0 +1,16 @@
+package myjava;
+
+public class MyJava {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Java");
+		
+		for (char x=0x0370; x<0x999;x++) {
+
+			System.out.println(x);
+		}
+
+	}
+
+}
